e6226375ba84f623a9a9be10daa2cc8f240dd836
Radzz Offc <radzzoffc@gmail.com>
2025-02-09 15:19:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // **SITE KEY DI-HIDE MENGGUNAKAN ENCODING**
 const encodedKey = "MHg0QUFBQUFBQThHcDFmT25GcjVZYmZwCg=="; // Base64 Encoded Key
-const siteKey = atob(encodedKey).replace(/XyWRootenZz\|my\|id/g, "-");
+const siteKey = atob(encodedKey);
 
 // Inject Turnstile Captcha ke halaman
 document.getElementById("captcha-container").innerHTML = `<div class="cf-turnstile" data-sitekey="${siteKey}"></div>`;
